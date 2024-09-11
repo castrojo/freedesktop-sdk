castrojo/freedesktop-sdk

commit 81155746702e278b8476e3166f3e41836969df75

linter: Fix utils/jsontomd.py

utils/jsontomd.py
@@ -17,16 +17,20 @@ args = parser.parse_args()
 
 json_data = json.loads(args.json.read())
 
-with open(args.json.name.replace('json', 'md'), 'w+') as md_file:
+with open(
+    args.json.name.replace('json', 'md'),
+    'w+',
+    encoding="utf-8"
+) as md_file:
     for obj in json_data['modules']:
         if 'name' in obj:
-            md_file.write('## {} \n'.format(obj['name']))
+            md_file.write('## {obj["nam"]} \n')
         if 'sources' in obj:
             for source in obj['sources']:
                 if source['type'] == 'archive' or source['type'] == 'git' or source['type'] == 'git_tag':
-                    md_file.write('  - {} \n'.format(source['url']))
+                    md_file.write('  - {source["url"]} \n')
                 if 'sha256' in source:
-                    md_file.write('  - {} \n'.format(source['sha256']))
+                    md_file.write('  - {source["sha256"]} \n')
                 if 'commit' in source:
-                    md_file.write('  - {} \n'.format(source['commit']))
+                    md_file.write('  - {source["commit"]} \n')
         md_file.write('\n')

commit 45dc0a237a4bf82daa0f9fd599268a009720df97

jsontomd: Use f-strings
Otherwise this is literally writing whatever is in quotes. Broken since
81155746702e278b8476e3166f3e41836969df75

Also fix dict key name

utils/jsontomd.py
@@ -24,13 +24,13 @@ with open(
 ) as md_file:
     for obj in json_data['modules']:
         if 'name' in obj:
-            md_file.write('## {obj["nam"]} \n')
+            md_file.write(f'## {obj["name"]} \n')
         if 'sources' in obj:
             for source in obj['sources']:
                 if source['type'] in ('archive', 'git', 'git_repo'):
-                    md_file.write('  - {source["url"]} \n')
+                    md_file.write(f'  - {source["url"]} \n')
                 if 'sha256' in source:
-                    md_file.write('  - {source["sha256"]} \n')
+                    md_file.write(f'  - {source["sha256"]} \n')
                 if 'commit' in source:
-                    md_file.write('  - {source["commit"]} \n')
+                    md_file.write(f'  - {source["commit"]} \n')
         md_file.write('\n')