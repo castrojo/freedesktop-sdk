2efe4f2c419f85601006e8a01150ed3008a5a9a9
bbhtt <bbhtt.zn0i8@slmail.me>
2024-09-15 09:25:42 +0530

    tests: Add a small test to check libheif plugin paths
    To make sure we don't regress
    
    (cherry picked from commit ac620c2ae9f4568e00bbe6cab33e6f43f10fcc3c)

---
+++ tests/libheif_plugin.cc
@@ -0,0 +1,43 @@
+#include "libheif/heif.h"
+#include <iostream>
+#include <optional>
+#include <string>
+#include <cstring>
+
+std::optional<std::string> getArg(int argc, char* argv[]) {
+    if (argc > 1) {
+        return std::string(argv[1]);
+    }
+    return std::nullopt;
+}
+
+int main(int argc, char* argv[]) {
+    auto plugin_paths = heif_get_plugin_directories();
+
+    if (plugin_paths == nullptr || plugin_paths[0] == nullptr) {
+        std::cout << "Plugin path not found\n";
+        heif_free_plugin_directories(plugin_paths);
+        return 1;
+    }
+
+    std::optional<std::string> firstArg = getArg(argc, argv);
+
+    if (!firstArg) {
+        std::cout << "No argument provided\n";
+        heif_free_plugin_directories(plugin_paths);
+        return 1;
+    }
+
+    std::cout << "Plugin path is " << plugin_paths[0] << "\n";
+    std::cout << "Argument is " << *firstArg << "\n";
+
+    if (strcmp(plugin_paths[0], firstArg->c_str()) != 0) {
+        std::cout << "Plugin path does not match argument\n";
+        heif_free_plugin_directories(plugin_paths);
+        return 1;
+    }
+
+    std::cout << "Plugin path matches argument\n";
+    heif_free_plugin_directories(plugin_paths);
+    return 0;
+}
