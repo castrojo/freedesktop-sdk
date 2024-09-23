ac174a82d2e4dae3d2acf25c1cafe11cfbea6c32
bbhtt <bbhtt.zn0i8@slmail.me>
2024-09-23 23:14:27 +0530

    news_validator: Add some more validations and stricter date validation

--- utils/news_validator.py
+++ utils/news_validator.py
@@ -10,27 +10,28 @@ import ruamel.yaml
 FD_SDK_TAG_FORMAT = r"^freedesktop-sdk-\d{2}\.08(?:beta|rc)?\.\d+(?:\.\d+)?$"
 
 
-def validate_date(date_text):
-    try:
-        datetime.date.fromisoformat(date_text)
-    except ValueError as err:
-        raise ValueError("Incorrect date format") from err
-
-
 def validate(args):
     yaml = ruamel.yaml.YAML()
+    tag_list = []
     with open(args.path, encoding="utf-8") as yaml_in:
         news_obj = yaml.load_all(yaml_in)
         for item in news_obj:
             news_dict = json.loads(json.dumps(item))
-            assert all(k in news_dict for k in ("Version", "Date", "Description"))
+            assert all(
+                k in news_dict.keys() for k in ("Version", "Date", "Description")
+            )
+            assert list(news_dict.keys())[0] == "Version"
+            assert list(news_dict.keys())[1] == "Date"
+            assert list(news_dict.keys())[2] == "Description"
             tag = news_dict["Version"]
+            tag_list.append(tag)
             tag_date = news_dict["Date"]
             desc = [i.strip() for i in news_dict["Description"].split("\n")]
             assert re.match(FD_SDK_TAG_FORMAT, tag)
-            validate_date(tag_date)
+            datetime.datetime.strptime(tag_date, "%Y-%m-%d")
             assert len(desc) >= 1
             assert desc[0].startswith(f"Changes in {tag}")
+    assert len(tag_list) == len(set(tag_list))
 
 
 def main():
