478fe1339a74df14319468cdfb0350523f37c0aa
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2024-11-10 21:42:11 +0200

    Add absolutely minimal wheel build tool
    This allows getting rid of some hacks bootstrapping pyproject tooling

---
+++ files/python-bootstrap/build.py
@@ -0,0 +1,26 @@
+import argparse
+import importlib
+import os
+import sys
+import tomllib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--no-isolation", action="store_true", help="no-op")
+parser.add_argument("--wheel", action="store_true", help="no-op")
+parser.add_argument("--outdir", default="dist")
+parser.add_argument("workdir")
+
+
+if __name__ == "__main__":
+    arguments = parser.parse_args()
+    os.chdir(arguments.workdir)
+    with open("pyproject.toml", "rb") as pyproject:
+        config = tomllib.load(pyproject)
+        backend_path = config["build-system"].get("backend-path")
+        if backend_path:
+            sys.path.extend(backend_path)
+        module = importlib.import_module(
+            config["build-system"]["build-backend"]
+        )
+        os.makedirs(arguments.outdir)
+        module.build_wheel(arguments.outdir)
