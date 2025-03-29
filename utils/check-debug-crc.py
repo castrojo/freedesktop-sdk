b9befc61031a42a59cc3145c201beee6b4c1740a
bbhtt <bbhtt.zn0i8@slmail.me>
2025-03-29 10:18:47 +0530

    check-debug-crc: Check for missing debug data too and fail if not found
    adds an ignore system too
    
    Closes https://gitlab.com/freedesktop-sdk/freedesktop-sdk/-/issues/1684

--- utils/check-debug-crc.py
+++ utils/check-debug-crc.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
 import argparse
+import json
 import os.path
 import subprocess
 import sys
 import tempfile
 import zlib
 
+
+def load_ignore_list(path: str):
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            return set(json.load(f))
+    return set()
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--verbose", action="store_true")
+parser.add_argument(
+    "--ignore-file",
+    default="missing-debug-data-allowlist.json",
+    help="JSON allowlist for ELFs with missing debug data",
+)
 parser.add_argument("sysroot")
 args = parser.parse_args()
 
 failed = False
+no_debug_objs: set[str] = set()
 
 for root, _, files in os.walk(args.sysroot):
     for f in files:
@@ -60,17 +75,28 @@ for root, _, files in os.walk(args.sysroot):
 
         if not os.path.exists(full_debugpath):
             print(f"{obj}: no debug")
-            continue
+            no_debug_objs.add(os.path.basename(obj))
 
-        with open(full_debugpath, "rb") as f:
-            debugdata = f.read()
-            calculated_crc = zlib.crc32(debugdata, 0)
+        if os.path.exists(full_debugpath):
+            with open(full_debugpath, "rb") as f:
+                debugdata = f.read()
+                calculated_crc = zlib.crc32(debugdata, 0)
 
-        if calculated_crc != crc:
-            print(f"{obj}: expected {crc:08x}, got {calculated_crc:08x}")
-            failed = True
-        elif args.verbose:
-            print(f"{obj}: crc {crc:08x} correct")
+            if calculated_crc != crc:
+                print(f"{obj}: expected {crc:08x}, got {calculated_crc:08x}")
+                failed = True
+            elif args.verbose:
+                print(f"{obj}: crc {crc:08x} correct")
+
+
+ignore_list = load_ignore_list(args.ignore_file)
+if ignore_list:
+    print(f"Found ignore list: {ignore_list}")
+    no_debug_objs -= ignore_list
+
+if no_debug_objs:
+    print(f"ELFs with no debug data: {no_debug_objs}")
+    failed = True
 
 if failed:
     sys.exit(1)
