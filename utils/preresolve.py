24111a5ab0b9ce935c8639c0f829d5f0130dc0fb
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2025-04-12 11:50:55 +0300

    Remove obsolete preresolve tool
    This is not actually used anymore

--- utils/preresolve.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-from socket import gethostbyname
-
-host = sys.argv[1]
-
-with open("/etc/hosts", "a", encoding="utf-8") as file:
-    print(gethostbyname(host) + " " + host, file=file)
