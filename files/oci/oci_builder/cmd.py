02505f92e144e561d94c92cdafc933064fcdf3db
bbhtt <bbhtt.zn0i8@slmail.me>
2026-03-06 06:55:28 +0530

    Merge nested if statements

--- files/oci/oci_builder/cmd.py
+++ files/oci/oci_builder/cmd.py
@@ -37,9 +37,8 @@ def main():
     data = yaml.load(sys.stdin, Loader=yaml.CLoader)
     compression = data.get("gzip", Compression.gzip)
     compression_level = data.get("compression-level")
-    if compression_level is None:
-        if compression == Compression.gzip:
-            compression_level = 5
+    if compression_level is None and compression == Compression.gzip:
+        compression_level = 5
     if compression not in Compression:
         raise RuntimeError("Compression must be in " + ",".join(Compression))
 
