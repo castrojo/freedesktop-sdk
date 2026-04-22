b49f32c696da13ec4739d35ff0547cbee497b031
Aleix Pol Gonzalez <aleix.pol@codethink.co.uk>
2026-04-22 16:20:22 +0200

    utils/test_*_system.py: wrap main calls in the __main__ case

--- utils/test_secure_system.py
+++ utils/test_secure_system.py
@@ -57,7 +57,8 @@ DIALOGS = {
     ],
 }
 
-result = system_test.main(
-    "Test that a minimal secure VM image works as expected", DIALOGS
-)
-sys.exit(result)
+if __name__ == "__main__":
+    result = system_test.main(
+        "Test that a minimal secure VM image works as expected", DIALOGS
+    )
+    sys.exit(result)
